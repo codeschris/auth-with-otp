70721e06ca4006f26e86f991589dce5d1ace016a
codeschris <chrismlolwa42@outlook.com>
2023-06-12 18:32:51 +0300

    creating and configuring database

--- python alt/main.py
+++ python alt/main.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, redirect, url_for, request
+import psycopg2
+import os
 import string
 import random
+
+#connect to the database using sqlalchemy
+
+def get_db_conn():
+    conn = psycopg2.connect(host='localhost',
+                            database='flask_db',
+                            user=os.environ['DB_USERNAME'],
+                            password=os.environ['DB_PASSWORD'])
+    return conn
 
 app = Flask(__name__)
 
@@ -10,7 +21,7 @@ def index():
     if request.method == 'POST':
         if request.form['username'] == '' or request.form['password'] == '':
             error = "Missing input. Input missing field(s)!"
-        elif request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        elif request.form['username'] != 'admin' or request.form['password'] != 'admin':    #change arguments for user and pass
             #generating lowercase and uppercase letters
             bets_low = string.ascii_lowercase
             bets_upper = string.ascii_uppercase
@@ -22,7 +33,7 @@ def index():
 
             error = 'OTP code is: {}'.format(result)    #passing OTP code incase of invalid details or forgotten password
         else:
-            return redirect(url_for('index.html'))
+            return redirect(url_for('index'))
         
     return render_template("index.html", error=error)
 
